9ed37a5cc66bbe420b485b5d5fb36ad6ff921a77
Anton Wyrowski <anton.wyrowski@gmail.com>
2022-03-20 14:50:38 +0100

    Initial Commit

---
+++ Shared/swift_data_flow_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_data_flow_exampleApp.swift
+//  Shared
+//
+//  Created by Anton Wyrowski on 20.03.22.
+//
+
+import SwiftUI
+
+@main
+struct swift_data_flow_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
